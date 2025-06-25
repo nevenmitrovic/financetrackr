b966d3009f1c0f051a05e36781540af83bb4aad4
neven <nevenmitrovic4@gmail.com>
2025-06-25 13:10:59 +0200

    income managment html

--- src/components/dashboard-content/income-managment/IncomeManagment.tsx
+++ src/components/dashboard-content/income-managment/IncomeManagment.tsx
@@ -1,7 +1,35 @@
+import { FaPlus } from 'react-icons/fa6'
+import { AiFillEdit } from 'react-icons/ai'
+
 import './income-managment.style.css'
 
 const IncomeManagment = () => {
-	return <div className='content-card income-managment'>Income managment</div>
+	return (
+		<div className='content-card income-managment'>
+			<div className='income-managment-header'>
+				<h3>Income Managment</h3>
+				<p>Montly income recap</p>
+				<div className='income-controller'>
+					<h3>3350.00$</h3>
+					<div>
+						<button>
+							<FaPlus />
+							Add
+						</button>
+						<button>
+							<AiFillEdit />
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className='income-managment-view'>
+				<p>Income breakdown</p>
+				<div className='part-time'>part-time</div>
+				<div className='paycheck'>paycheck</div>
+				<div className='gift'>gift</div>
+			</div>
+		</div>
+	)
 }
 
 export default IncomeManagment
