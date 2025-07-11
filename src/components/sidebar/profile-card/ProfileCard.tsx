b3dc9f8dadb77bd4869f5f50535b88a14a85fc7e
neven <nevenmitrovic4@gmail.com>
2025-07-11 14:39:09 +0200

    supabaseClient and initial  view for signin

--- src/components/sidebar/profile-card/ProfileCard.tsx
+++ src/components/sidebar/profile-card/ProfileCard.tsx
@@ -1,13 +1,18 @@
+import { useEffect, useRef, useState } from 'react'
 import { useSidebarContext } from '@/contexts/SidebarContext'
 import { CgArrowsV } from 'react-icons/cg'
+import { supabaseClient } from '@/services/supabaseClient'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
 import avatar from '@/assets/images/avatar.png'
 import './profile-card.style.css'
-import { useEffect, useRef, useState } from 'react'
 
 const ProfileCard = () => {
 	const { open, toggleOpen } = useSidebarContext()
 	const [openDropdown, setOpenDropdown] = useState(false)
+	const [session, setSession] = useState<any | null>(null)
+
 	const profileCardRef = useRef<HTMLDivElement>(null)
 
 	const handleDropdown = () => {
@@ -15,14 +20,34 @@ const ProfileCard = () => {
 
 		setOpenDropdown((prev) => !prev)
 	}
+	const handleClickOutside = (event: MouseEvent) => {
+		if (profileCardRef.current && !profileCardRef.current.contains(event.target as Node)) {
+			setOpenDropdown(false)
+		}
+	}
+	async function handleSignInWithGoogle(response: any) {
+		const { data, error } = await supabaseClient.auth.signInWithIdToken({
+			provider: 'google',
+			token: response.credential,
+		})
+
+		if (!error) setSe
+	}
 
 	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (profileCardRef.current && !profileCardRef.current.contains(event.target as Node)) {
-				setOpenDropdown(false)
-			}
-		}
+		supabaseClient.auth.getSession().then(({ data: { session } }) => {
+			setSession(session)
+		})
+		const {
+			data: { subscription },
+		} = supabaseClient.auth.onAuthStateChange((_event, session) => {
+			setSession(session)
+		})
 
+		return () => subscription.unsubscribe()
+	}, [])
+
+	useEffect(() => {
 		if (openDropdown) {
 			document.addEventListener('mousedown', handleClickOutside)
 		}
@@ -31,6 +56,31 @@ const ProfileCard = () => {
 			document.removeEventListener('mousedown', handleClickOutside)
 		}
 	}, [openDropdown])
+
+	if (!session) {
+		console.log(session)
+
+		return (
+			<div
+				className='sidebar-card'
+				onClick={async () =>
+					await supabaseClient.auth.signInWithOAuth({
+						provider: 'google',
+					})
+				}
+			>
+				<img
+					className={`avatar ${!open ? 'margin-auto' : ''}`}
+					src={avatar}
+					alt='financetrackr logo'
+				/>
+				<div className={`profile-card-title ${!open ? 'hidden' : ''}`}>
+					<h3>Signin</h3>
+					<p>with google</p>
+				</div>
+			</div>
+		)
+	}
 
 	return (
 		<div
