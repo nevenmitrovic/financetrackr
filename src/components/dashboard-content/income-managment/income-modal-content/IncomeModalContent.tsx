29308289eee837cc41717d884d58edd419921287
neven <nevenmitrovic4@gmail.com>
2025-07-18 18:03:52 +0200

    incomeModalContent changed UI

--- src/components/dashboard-content/income-managment/income-modal-content/IncomeModalContent.tsx
+++ src/components/dashboard-content/income-managment/income-modal-content/IncomeModalContent.tsx
@@ -3,36 +3,45 @@ import { useIncomeContext } from '@/contexts/IncomeManagmentContext'
 
 import './income-modal-content.style.css'
 
-const IncomeModalContent = () => {
-	const [incomeType, setIncomeType] = useState('')
-	const { toggleModal } = useIncomeContext()
+// USE THIS FOR PATCH INCOME DATA
+{
+	/* <label htmlFor='incomeType'>Select the income type</label>
+<select
+	id='incomeType'
+	name='incomeType'
+	className='button'
+	value={incomeType}
+	onChange={handleIncomeTypeChange}
+>
+	<option value=''>-- Select --</option>
+	<option value='partTime'>Part-Time</option>
+	<option value='paycheck'>Paycheck</option>
+	<option value='gift'>Gift</option>
+</select> */
+	//	const [incomeType, setIncomeType] = useState('')
+	//	const handleIncomeTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+	//	setIncomeType(e.target.value)
+	// }
+}
 
-	const handleIncomeTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-		setIncomeType(e.target.value)
-	}
+const IncomeModalContent = () => {
+	const { toggleModal } = useIncomeContext()
 
 	return (
 		<>
 			<h3 className='income-modal-header'>Add income for the current month</h3>
 			<div className='income-modal-body'>
 				<div>
-					<label htmlFor='incomeType'>Select the income type</label>
-					<select
-						id='incomeType'
-						name='incomeType'
-						className='button'
-						value={incomeType}
-						onChange={handleIncomeTypeChange}
-					>
-						<option value=''>-- Select --</option>
-						<option value='partTime'>Part-Time</option>
-						<option value='paycheck'>Paycheck</option>
-						<option value='gift'>Gift</option>
-					</select>
+					<label htmlFor='partTime'>Part-Time</label>
+					<input type='number' name='partTime' className='input' />
 				</div>
 				<div>
-					<label htmlFor='addIncome'>Add income value</label>
-					<input type='number' name='addIncome' className='input' />
+					<label htmlFor='paycheck'>Paycheck</label>
+					<input type='number' name='paycheck' className='input' />
+				</div>
+				<div>
+					<label htmlFor='gift'>Gift</label>
+					<input type='number' name='gift' className='input' />
 				</div>
 			</div>
 			<div className='income-modal-footer'>
