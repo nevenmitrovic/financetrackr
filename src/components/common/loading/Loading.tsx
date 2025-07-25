15ba39e2bbf4639bc31aacbef7652662b9d7505b
neven <nevenmitrovic4@gmail.com>
2025-07-25 13:17:08 +0200

    loading spinner UI changed

--- src/components/common/loading/Loading.tsx
+++ src/components/common/loading/Loading.tsx
@@ -1,14 +1,16 @@
 import { DotLoader } from 'react-spinners'
+import { useIsFetching } from '@tanstack/react-query'
 
 import './loading.style.css'
-import { useIsFetching } from '@tanstack/react-query'
 
 const Loading = () => {
 	const isFetching = useIsFetching()
 
+	if (isFetching === 0) return null
+
 	return (
-		<div className={`loading-container ${!isFetching ? 'hidden' : ''}`}>
-			<DotLoader size={100} speedMultiplier={2} color='var(--clr-text-primary)' />
+		<div className='loading-overlay'>
+			<DotLoader size={100} speedMultiplier={2} color='white' />
 		</div>
 	)
 }
